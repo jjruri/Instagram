1b217c02b50d781975a8385699a1aa834dcec3fc
Junya.Satou <jj.ruri11280506@gmail.com>
2020-02-01 13:00:39 +0900

    tabBarControllerに投稿のときだけモーダルで表示させるように処理を記載した

---
+++ Instagram/TabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  TabBarController.swift
+//  Instagram
+//
+//  Created by 佐藤淳哉 on 2020/02/01.
+//  Copyright © 2020 Junya.Satou. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController,UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.delegate = self
+    }
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        //投稿がタップされた時だけは画面切り替えじゃなくいモーダルで表示させる
+        if viewController is ImageSelectViewController {
+            let imageSelectViewController = storyboard!.instantiateViewController(withIdentifier: "ImageSelect")
+            present(imageSelectViewController, animated: true/* 遷移の時のアニメーションはデフォルトの下からでるやつで*/, completion: nil/* 完了後になにかするか⇨しない*/)
+            return false
+        }
+        else{//投稿以外のタブがタップされた場合は普通に処理していいよ
+            return true
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
