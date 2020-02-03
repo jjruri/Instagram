32633066a3dc514d021c7485341dcbe6554cbf37
Junya.Satou <jj.ruri11280506@gmail.com>
2020-02-03 10:59:06 +0900

    カメラボタンの記述をしたが、強烈にクラッシュ。カメラの使用許諾を利用者にとっていないことが原因。とわかったところまで。

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -20,6 +20,12 @@ class ImageSelectViewController: UIViewController,UIImagePickerControllerDelegat
     }
     
     @IBAction func handleCameraButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .camera
+            self.present(pickerController,animated: true,completion: nil)
+        }
     }
     
     @IBAction func handleCancelButton(_ sender: Any) {
@@ -28,19 +34,19 @@ class ImageSelectViewController: UIViewController,UIImagePickerControllerDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
