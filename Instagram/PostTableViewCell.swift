fb1f94aa9914c19a4489296c7dacd94cf507ab52
Junya.Satou <jj.ruri11280506@gmail.com>
2020-02-04 17:12:55 +0900

    postTableViewCellをつくってUIを決めたところまで（コメントを含む） postdataはまだ作ってない

---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by 佐藤淳哉 on 2020/02/04.
+//  Copyright © 2020 Junya.Satou. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
